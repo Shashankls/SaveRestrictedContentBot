ef21fe3a9eccdc92e164b07886280e1423a9cc3e
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-25 21:59:18 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,6 +15,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID","29400566")
 API_HASH = config("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f")
 AUTH = config("AUTH","6084381887")
+FORCESUB = config("FORCESUB","
 BOT_TOKEN = config("BOT_TOKEN","6653186136:AAElR4rlBQi5JrIdltqvHLpPc8y1MmMeh10")
 SESSION = config("SESSION","BQHAnfYABKr5ctlNYKgQEJ_VSKA89UPCtmnBJ0Bfl5FenxnonVBJ74ETSYB-r1Ou3DRXLrQvsUcIC4jS4_0980rTWUfop2TawTOHLUmBd5Hkzib7DNeb6jjRTMfSQN630KyzKoWfiAYsRDqpNFh86IEH5WeZ1PIGVMMmvUIDyOaN3owqJ3DvKO8Bu3F1y8RtTvTHWlZEnTKiKG-MCpy9YwX9ihRNOPUbGDAuqCCpLUY4tMeklFDzs0sM-Vtyw31hwqNOec5RS3AvVeQO501jFSxiSGB_vWmlmdqXDBwpkLaifnqOPOofcsg3IsARO5x1lMDsaGbTZCcgl9qh4pYtYEFqx2oDqAAAAAGMj5BYAQ")
 
